0d8e0af78e295c39a245455943aa879086a8f156
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2024-08-23 17:26:39 +0300

    feat: add dark mode feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "../components/theme-provider";
+import Header from "@/components/shared/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body className="min-h-screen min-w-screen">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <div className="min-h-screen min-w-screen bg-neutral">{children}</div>
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   );
 }
---
+++ src/components/shared/header.tsx
@@ -0,0 +1,14 @@
+import { ModeSwitch } from "./dark-mode-switch";
+
+export default function Header(){
+    return (
+        <div className="h-20 min-w-full min-h-full shadow-md rounded-md md:flex md:justify-between px-10 py-3 md:items-center">
+            <div>
+                Header
+            </div>
+            <div>
+                <ModeSwitch />
+            </div>
+        </div>
+    )
+}
