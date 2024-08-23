b2450d98cb9e49b1293e69a0493302c930cff98b
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2024-08-23 17:27:05 +0300

    feat: add dark mode feature (#2)

---
+++ src/components/shared/dark-mode-switch.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Switch } from "../ui/switch";
+
+export function ModeSwitch() {
+  const { setTheme } = useTheme();
+  const [isDarkMode, setisDarkMode] = React.useState(false);
+
+  function onSwithChange() {
+    if (isDarkMode) {
+      setTheme("light");
+      setisDarkMode(false);
+    } else {
+      setTheme("dark");
+      setisDarkMode(true);
+    }
+  }
+
+
+  return (
+    <>
+      <div className="flex justify-center items-center gap-2">
+      <Sun className="h-[1.2rem] w-[1.2rem]" />
+        <Switch checked={isDarkMode} onCheckedChange={onSwithChange} />
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      </div>
+    </>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,27 +19,62 @@ const config = {
     },
     extend: {
       colors: {
-        'primary-main': 'var(--color-primary-main)',
-        'primary-hovered': 'var(--color-primary-hovered)',
-        'primary-disabled': 'var(--color-primary-disabled)',
-        'secondary-main': 'var(--color-secondary-main)',
-        'secondary-hovered': 'var(--color-secondary-hovered)',
-        'secondary-disabled': 'var(--color-secondary-disabled)',
-        'tertiary-main': 'var(--color-tertiary-main)',
-        'tertiary-hovered': 'var(--color-tertiary-hovered)',
-        'tertiary-disabled': 'var(--color-tertiary-disabled)',
-        'red-main': 'var(--color-red-main)',
-        'red-hovered': 'var(--color-red-hovered)',
-        'red-disabled': 'var(--color-red-disabled)',
-        'green-main': 'var(--color-green-main)',
-        'green-hovered': 'var(--color-green-hovered)',
-        'green-disabled': 'var(--color-green-disabled)',
-        'blue-main': 'var(--color-blue-main)',
-        'blue-hovered': 'var(--color-blue-hovered)',
-        'blue-disabled': 'var(--color-blue-disabled)',
-        'yellow-main': 'var(--color-yellow-main)',
-        'yellow-hovered': 'var(--color-yellow-hovered)',
-        'yellow-disabled': 'var(--color-yellow-disabled)',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        switch: {
+          background: "var(--color-switch-background)"
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        primary: {
+          main: 'var(--color-primary-main)',
+          hovered: 'var(--color-primary-hovered)',
+          disabled: 'var(--color-primary-disabled)'
+        },
+        secondary: {
+          main: 'var(--color-secondary-main)',
+          hovered: 'var(--color-secondary-hovered)',
+          disabled: 'var(--color-secondary-disabled)'
+        },
+        tertiary: {
+          main: 'var(--color-tertiary-main)',
+          hovered: 'var(--color-tertiary-hovered)',
+          disabled: 'var(--color-tertiary-disabled)'
+        },
+        red: {
+          main: 'var(--color-red-main)',
+          hovered: 'var(--color-red-hovered)',
+          disabled: 'var(--color-red-disabled)'
+        },
+        green: {
+          main: 'var(--color-green-main)',
+          hovered: 'var(--color-green-hovered)',
+          disabled: 'var(--color-green-disabled)'
+        },
+        blue: {
+          main: 'var(--color-blue-main)',
+          hovered: 'var(--color-blue-hovered)',
+          disabled: 'var(--color-blue-disabled)'
+        },
+        yellow: {
+          main: 'var(--color-yellow-main)',
+          hovered: 'var(--color-yellow-hovered)',
+          disabled: 'var(--color-yellow-disabled)'
+        },
+        neutral: 'var(--color-neutral)'
       },
       borderRadius: {
         lg: "var(--radius)",
