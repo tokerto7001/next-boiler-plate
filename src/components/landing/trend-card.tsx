4d503f97bf5c3673c4e6793df08ff62e32faef20
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2024-09-02 11:25:03 +0300

    refactor: redesign blog top rated cards

---
+++ src/components/landing/trend-card.tsx
@@ -0,0 +1,40 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "../ui/button";
+import Image, { StaticImageData } from "next/image";
+
+interface ITrendCardProps {
+  data: {
+    title: string;
+    content: string;
+    image: StaticImageData
+  };
+}
+
+export default function TrendCard({ data }: ITrendCardProps) {
+  return (
+    <Card className="flex flex-col gap-3 hover:bg-neutral p-6 rounded-xl hover:cursor-pointer md:w-1/3">
+        <CardHeader className="h-1/5 flex items-center justify-center">
+          <CardTitle className="text-center text-md  lg:text-xl md:text-md">{data.title}</CardTitle>
+        </CardHeader>
+        <CardContent className="h-3/6 flex justify-center items-center">
+            <div className="relative h-36 w-full">
+                <Image
+                src={data.image}
+                alt="Blog Image"
+                fill={true}
+                className="object-cover"
+                />
+            </div>
+        </CardContent>
+        <CardFooter className="h-1/5 flex items-center justify-center">
+            <Button className="bg-primary-main text-white hover:bg-primary-hovered disabled:bg-primary-disabled">Read Now!</Button>
+        </CardFooter>
+    </Card>
+  );
+}
