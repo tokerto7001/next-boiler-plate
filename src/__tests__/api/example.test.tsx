90258b0d7b44be216bbfcdd815802e40d3cefb92
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2024-09-06 08:44:13 +0300

    test: add failed test for the example api route

--- src/__tests__/api/example.test.tsx
+++ src/__tests__/api/example.test.tsx
@@ -5,11 +5,26 @@ import { expect, test } from "vitest";
 test('Example API Route GET SUCCESS Test', async() => {
     const generatedRequest = generateNextRequest('/example', {
         method: 'GET',
+        queryParams: {
+            example: 'example'
+        }
     });
 
     const response = exampleRoute(generatedRequest);
     const resObject = await response.json();
     expect(response.status).toBe(200);
     expect(resObject.message).toBe('Hello from example');
+
+});
+
+test('Example API Route GET FAILED Test', async() => {
+    const generatedRequest = generateNextRequest('/example', {
+        method: 'GET',
+    });
+
+    const response = exampleRoute(generatedRequest);
+    const resObject = await response.json();
+    expect(response.status).toBe(400);
+    expect(resObject.message).toBe('Bad Request');
 
 });
--- src/app/api/example.route.ts
+++ src/app/api/example.route.ts
@@ -2,9 +2,21 @@ import { NextRequest } from "next/server";
 
 export function GET(req: NextRequest) {
 
-    return Response.json({
-        message: 'Hello from example'
-    }, {
-        status: 200
-    });
+    const queryParams = req.nextUrl.searchParams;
+    const exampleParam = queryParams.get('example');
+
+    if(exampleParam) {
+        return Response.json({
+            message: 'Hello from example'
+        }, {
+            status: 200
+        });
+    } else {
+        return Response.json({
+            message: 'Bad Request'
+        }, {
+            status: 400
+        });
+    }
+
 }
